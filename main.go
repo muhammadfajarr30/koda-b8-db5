b9c1bf898f354bc6a46181478a1ee6f6a8b4160e
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-07-15 15:03:35 +0700

    feat: implement contact management functions for adding and listing contacts

--- main.go
+++ main.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"koda-b8-db5/lib"
+	"koda-b8-db5/models"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func showlistContacts(conn *pgx.Conn) {
+
+	listContacts, err := models.GetAllContacts(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, c := range listContacts {
+		fmt.Printf("ID    : %d\n", c.Id)
+		fmt.Printf("Email : %s\n", c.Email)
+		fmt.Printf("Phone : %s\n", c.Phone)
+		fmt.Println("-----------------------")
+	}
+
+}
+
+func addContact(conn *pgx.Conn) {
+	var contact models.Contact
+
+	fmt.Print("Email : ")
+	fmt.Scan(&contact.Email)
+
+	fmt.Print("Phone : ")
+	fmt.Scan(&contact.Phone)
+
+	err := models.AddContact(contact, conn)
+	if err != nil {
+		log.Println("Gagal menambahkan contact:", err)
+		return
+	}
+
+	fmt.Println("Contact berhasil ditambahkan!")
+}
+
+func main() {
+	conn := lib.Conn()
+	defer conn.Close(context.Background())
+	var opt string
+	for {
+
+		fmt.Println("====== CONTACT LIST ======")
+		fmt.Println("1. Lihat Semua Kontak")
+		fmt.Println("2. Tambah Kontak")
+		fmt.Print("pilih: ")
+		fmt.Scan(&opt)
+
+		switch opt {
+		case "1":
+			showlistContacts(conn)
+		case "2":
+			addContact(conn)
+		}
+
+	}
+
+}
